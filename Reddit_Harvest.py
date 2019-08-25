029f7075a9a7eb4500f8ae1c6bbeb1cfb7ba168b
epicrunze <runze.zhang2001@gmail.com>
2019-08-24 21:57:52 -0400

    new big file

--- Reddit_Harvest.py
+++ Reddit_Harvest.py
@@ -1,16 +1,17 @@
 import praw
 import inspect
-i = 1
+#i = 1
 reddit = praw.Reddit(client_id='PurljHA0fr4yYQ',
                      client_secret='URh00WvOlXQz5O_06VhkBhIXtus',
                      user_agent='Maybeatestytestmaybeatroll')
 for submission in reddit.subreddit('TIFU').top(time_filter='all', limit=10):
-    filename = "Dataset/TIFUPostTop{}.txt".format(i)
-    filepath = open(filename,"w+")
+    #filename = "Dataset/TIFUPostTop{}.txt".format(i)
+    filename = "TIFUFull.txt"
+    filepath = open(filename,"a+")
     if not submission.selftext:
         continue
-    filepath.write(submission.selftext)
+    filepath.write(submission.selftext + "/n")
     filepath.close()
-    i += 1
+    #i += 1
 # 'selftext' is the attribute of the post 
 
