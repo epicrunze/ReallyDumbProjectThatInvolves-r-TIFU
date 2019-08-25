7dc0192f49a7b179d5d36b20c96ceaaecf5e551d
epicrunze <runze.zhang2001@gmail.com>
2019-08-24 22:17:55 -0400

    Bigger Dataset

--- Reddit_Harvest.py
+++ Reddit_Harvest.py
@@ -4,10 +4,10 @@ import inspect
 reddit = praw.Reddit(client_id='PurljHA0fr4yYQ',
                      client_secret='URh00WvOlXQz5O_06VhkBhIXtus',
                      user_agent='Maybeatestytestmaybeatroll')
-for submission in reddit.subreddit('TIFU').top(time_filter='all', limit=10):
+for submission in reddit.subreddit('TIFU').top(time_filter='all', limit=100):
     #filename = "Dataset/TIFUPostTop{}.txt".format(i)
     filename = "TIFUFull.txt"
-    filepath = open(filename,"a+")
+    filepath = open(filename,"a+",encoding="utf-8")
     if not submission.selftext:
         continue
     filepath.write(submission.selftext + "/n")
