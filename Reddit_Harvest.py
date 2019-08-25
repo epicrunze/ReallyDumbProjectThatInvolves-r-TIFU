9b1f0124cb310c12b18fd63681144a962327b13a
epicrunze <runze.zhang2001@gmail.com>
2019-08-24 23:53:50 -0400

    Added more TIFU

--- Reddit_Harvest.py
+++ Reddit_Harvest.py
@@ -4,9 +4,9 @@ import inspect
 reddit = praw.Reddit(client_id='PurljHA0fr4yYQ',
                      client_secret='URh00WvOlXQz5O_06VhkBhIXtus',
                      user_agent='Maybeatestytestmaybeatroll')
-for submission in reddit.subreddit('entitledparents').top(time_filter='all', limit=5000):
+for submission in reddit.subreddit('TIFU').top(time_filter='all', limit=10000):
     #filename = "Dataset/TIFUPostTop{}.txt".format(i)
-    filename = "IntingParents.txt"
+    filename = "TIFU.txt"
     filepath = open(filename,"a+",encoding="utf-8")
     if not submission.selftext:
         continue
